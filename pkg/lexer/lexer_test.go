b109514904e87b6a4652cb56a522b741a1ab982c
Winterson <u19cse1028@cit.ac.in>
2024-12-30 20:21:11 +0530

    extend lexer test

--- pkg/lexer/lexer_test.go
+++ pkg/lexer/lexer_test.go
@@ -6,27 +6,81 @@ import (
 )
 
 func TestNextToken(test *testing.T) {
-	input := `(+=(){},;)`
-	tests := []struct {
+	/*
+		test1_input := `(+=(){},;)`
+
+		tests  := []struct {
+			expectedType    token.TokenType
+			expectedLiteral string
+		}{
+			{token.LPAREN, "("},
+			{token.PLUS, "+"},
+			{token.ASSIGN, "="},
+			{token.LPAREN, "("},
+			{token.RPAREN, ")"},
+			{token.LBRACE, "{"},
+			{token.RBRACE, "}"},
+			{token.COMMA, ","},
+			{token.SEMICOLON, ";"},
+			{token.RPAREN, ")"},
+			{token.EOF, ""},
+		}
+	*/
+	test2_input := `let five = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+		x + y;
+	};
+
+	let result = add(five, ten);
+	`
+	tests2 := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
-		{token.LPAREN, "("},
-		{token.PLUS, "+"},
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.ASSIGN, "="},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
 		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
-	_lexer := NewLexer(input)
+	TEST_INPUT := test2_input
+	TEST_COLLECTION := tests2
+	_lexer := NewLexer(TEST_INPUT)
 
-	for index, testToken := range tests {
+	for index, testToken := range TEST_COLLECTION {
 		nextToken := _lexer.NextToken()
 		if nextToken.Type != testToken.expectedType {
 			test.Fatalf("tests[%d] - wrong tokentype. expected=%q, got=%q", index, testToken.expectedType, nextToken.Type)
