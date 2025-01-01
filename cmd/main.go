0b025ed2d0eb742eaade7291b25ed66f9c350b98
Winterson <u19cse1028@cit.ac.in>
2025-01-01 16:46:58 +0530

    cleanup main.go file to add repl start

--- cmd/main.go
+++ cmd/main.go
@@ -1,44 +1,18 @@
 package main
 
-import "fmt"
-
-type ServerState int
-
-const (
-	StateIdle ServerState = iota
-	StateConnected
-	StateError
-	StateRetrying
+import (
+	"fmt"
+	"interpreter/pkg/repl"
+	"os"
+	"os/user"
 )
 
-var stateName = map[ServerState]string{
-	StateIdle:      "idle",
-	StateConnected: "connected",
-	StateError:     "error",
-	StateRetrying:  "retrying",
-}
-
-func (serverState ServerState) String() string {
-	return stateName[serverState]
-}
-
 func main() {
-	initialState := transition(StateIdle)
-	fmt.Println(initialState)
-
-	finalState := transition(initialState)
-	fmt.Println(finalState)
-}
-
-func transition(serverState ServerState) ServerState {
-	switch serverState {
-	case StateIdle:
-		return StateConnected
-	case StateConnected, StateRetrying:
-		return StateIdle
-	case StateError:
-		return StateError
-	default:
-		panic(fmt.Errorf("unknown state: %s", serverState))
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
 	}
+	fmt.Printf("Hello %s! Welcome to the R-E-P Loop!\n", user.Username)
+	fmt.Printf("Type in the commands\n")
+	repl.REPLStart(os.Stdin, os.Stdout)
 }
