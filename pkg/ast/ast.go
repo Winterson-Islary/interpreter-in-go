8b2ebde771edef5103bdfdd92099b04a3ac1a95c
Winterson <u19cse1028@cit.ac.in>
2025-01-08 21:09:47 +0530

    baisc ast components setup

---
+++ pkg/ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import "interpreter/pkg/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+type Statement interface {
+	Node
+	statementNode()
+}
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (program *Program) TokenLiteral() string {
+	if len(program.Statements) > 0 {
+		return program.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (ident *Identifier) expressionNode() {}
+func (ident *Identifier) TokenLiteral() string {
+	return ident.Token.Literal
+}
